4201de9ed0c336432d82283743188a96c086afdb
Ali <jamaliali219@gmail.com>
2019-04-21 15:18:20 +0430

    Implement Ya Mahdi quick setting

---
+++ app/src/main/java/ir/anexception/islamicquicksettings/YaMahdiTileService.java
@@ -0,0 +1,28 @@
+package ir.anexception.islamicquicksettings;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class YaMahdiTileService extends TileService {
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+
+        getQsTile().setState(Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+
+        switch (getQsTile().getState()) {
+            case Tile.STATE_ACTIVE: {
+                break;
+            }
+            case Tile.STATE_INACTIVE: {
+                break;
+            }
+        }
+    }
+}
